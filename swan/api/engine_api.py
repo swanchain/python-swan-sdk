715b9c5c3b811b44dade4bfe3539d2a638e52292
arash khosravi <arash.khosravi@nbai.io>
2023-12-06 16:33:42 -0500

    Implement Black on all files

--- swan/api/engine_api.py
+++ swan/api/engine_api.py
@@ -2,27 +2,33 @@ from swan.api_client import APIClient
 from swan.common.constants import *
 import logging
 
+
 class EngineAPI(object):
     def __init__(self, api_client=None, api_key=None, is_calibration=False):
         if api_client is None:
-            api_client = APIClient("placeholder_api_key","sample_wallet_address")
+            api_client = APIClient("placeholder_api_key", "sample_wallet_address")
         self.api_client = api_client
         self.AUCTION_API = api_client.AUCTION_API
-        #TODO: once authentication added we can modify this
+        # TODO: once authentication added we can modify this
         self.token = self.api_client.token
-        #self.token = None
+        # self.token = None
 
     def get_celery_task_status(self, task_id):
-        params = {"task_id":task_id}
+        params = {"task_id": task_id}
         try:
-            response = self.api_client._request_with_params(GET,CELERY,self.api_client.AUCTION_API, params, self.token, None)
+            response = self.api_client._request_with_params(
+                GET, CELERY, self.api_client.AUCTION_API, params, self.token, None
+            )
             return response
         except:
             logging.error("An error occurred while executing get_celery_task_status()")
             return None
+
     def get_processing_tasks(self):
         try:
-            result = self.api_client._request_without_params(GET,PROCESSING_TASKS,self.api_client.AUCTION_API, self.token)
+            result = self.api_client._request_without_params(
+                GET, PROCESSING_TASKS, self.api_client.AUCTION_API, self.token
+            )
             return result
         except:
             logging.error("An error occurred while executing get_processing_tasks()")
@@ -30,7 +36,9 @@ class EngineAPI(object):
 
     def send_jobs(self, job_data):
         try:
-            response = self.api_client._request_with_params(POST,JOBS,self.api_client.AUCTION_API,job_data,self.token, None)
+            response = self.api_client._request_with_params(
+                POST, JOBS, self.api_client.AUCTION_API, job_data, self.token, None
+            )
             return response
         except:
             logging.error("An error occurred while executing send_jobs()")
