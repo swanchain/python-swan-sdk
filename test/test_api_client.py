cacc22fb35cc3a720479f4ec729f7cedf0d126f5
arash khosravi <arash.khosravi@nbai.io>
2023-12-07 11:24:31 -0500

    modify flow

---
+++ test/test_api_client.py
@@ -0,0 +1,32 @@
+import pytest
+import requests
+
+from swan.api_client import APIClient
+
+
+class TestAPIClient:
+    def test_init_with_api_key_and_wallet_address(self):
+        api_key = "test_api_key"
+        wallet_address = "test_wallet_address"
+        api_client = APIClient(api_key, wallet_address)
+
+        assert api_client.api_key == (api_key,)
+        assert api_client.wallet_address == (wallet_address,)
+
+    def test_api_key_and_wallet_address(self):
+        api_key = "12345"
+        wallet_address = "abcde"
+        api_client = APIClient(api_key, wallet_address)
+        assert api_client.api_key == (api_key,)
+        assert api_client.wallet_address == (wallet_address,)
+
+    def test_api_key_login_with_valid_credentials(self, mocker):
+        api_key = "12345"
+        wallet_address = "abcde"
+        api_client = APIClient(api_key, wallet_address)
+        mocker.patch.object(
+            api_client, "_request_with_params", return_value={"data": "token"}
+        )
+        token = api_client.api_key_login()
+        assert token == "token"
+
--- test/test_jobs.py
+++ test/test_jobs.py
@@ -1,17 +1,17 @@
-import pytest
-import requests_mock
-import requests
-from swan.common import constants as c
-
-
-class TestMockSendJob:
-    @pytest.fixture
-    def mock_requests(self, shared_real_engine_api):
-        self.engine_api = shared_real_engine_api
-        with requests_mock.Mocker() as m:
-            m.post(c.JOBS, json={"job1": "data", "job2": "data"})
-            yield m
-
-    def test_mock_job_send(self, mock_requests):
-        response = self.engine_api.send_jobs({"job1": "data", "job2": "data"})
-        assert response == {"job1": "data", "job2": "data"}
+# import pytest
+# import requests_mock
+# import requests
+# from swan.common import constants as c
+#
+#
+# class TestMockSendJob:
+#     @pytest.fixture
+#     def mock_requests(self, shared_real_engine_api):
+#         self.engine_api = shared_real_engine_api
+#         with requests_mock.Mocker() as m:
+#             m.post(c.JOBS, json={"job1": "data", "job2": "data"})
+#             yield m
+#
+#     def test_mock_job_send(self, mock_requests):
+#         response = self.engine_api.send_jobs({"job1": "data", "job2": "data"})
+#         assert response == {"job1": "data", "job2": "data"}
