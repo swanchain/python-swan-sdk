31c168632a57e37e8ebf53c33a113aefb4f2233f
arash khosravi <arash.khosravi@nbai.io>
2023-12-07 13:20:22 -0500

    Change names correctly, add comments

--- test/conftest.py
+++ test/conftest.py
@@ -1,9 +1,15 @@
-import pytest, os
+""" Test Configuration for Pytest. """
+
+import pytest
+import os
 from swan.api_client import APIClient
 from swan.api.engine_api import EngineAPI
 from dotenv import load_dotenv
 
+# Load environment variables from a .env file
 load_dotenv()
+
+# Retrieve environment variables
 api_key = os.getenv("api_key")
 wallet_address = os.getenv("wallet_address")
 wrong_wallet_address = os.getenv("wrong_wallet_address")
@@ -11,7 +17,12 @@ wrong_api_key = os.getenv("wrong_api_key")
 
 
 @pytest.fixture()
-def shared_real_engine_api(scope="module", autouse=True):
+def shared_real_engine_api():
+    """Fixture to provide a shared Engine API instance."""
+    # Initialize the API client with real credentials
     api_client = APIClient(api_key, wallet_address, True)
+
+    # Create an Engine API instance using the API client
     engine_api = EngineAPI(api_client)
+
     return engine_api
--- test/test_dashboards.py
+++ test/test_dashboards.py
@@ -1,3 +1,5 @@
+"""Tests for Mock Send Job functionality """
+
 import pytest
 import requests_mock
 from swan.common import constants as c
@@ -6,17 +8,31 @@ from swan.common import constants as c
 class TestMockSendJob:
     @pytest.fixture
     def mock_requests(self, shared_real_engine_api):
+        """Fixture to mock network requests for processing tasks.
+
+        Args:
+            shared_real_engine_api: A fixture providing a shared instance of the Engine API.
+
+        Yields:
+            A requests mocker object to intercept and mock responses for network requests.
+        """
         self.engine_api = shared_real_engine_api
         nested_data = {
             "task1": {"task_instance_data": "task_1_data"},
             "task2": {"task_instance_data": "task_2_data"},
         }
+
         with requests_mock.Mocker() as m:
+            # Mocking the GET request to the processing tasks endpoint
             m.get(c.PROCESSING_TASKS, json=nested_data)
             yield m
 
     def test_mock_processing_tasks(self, mock_requests):
+        """Test the mock processing tasks query."""
+        # Querying the mocked processing tasks
         response = self.engine_api.get_processing_tasks()
+
+        # Asserting that the response matches the mocked data
         assert response == {
             "task1": {"task_instance_data": "task_1_data"},
             "task2": {"task_instance_data": "task_2_data"},
