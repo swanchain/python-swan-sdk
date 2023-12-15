8212db5520e151c8d28b2e52e45dd8a4a2392187
arash khosravi <arash.khosravi@nbai.io>
2023-12-15 15:44:24 -0500

    Add get_collateral_balance function

--- src/api/cp.py
+++ src/api/cp.py
@@ -2,9 +2,14 @@
 
 import logging
 import requests
-from typing import List, Dict, Any
-from src.constants.constants import SWAN_API, ALL_CP_MACHINE
-from src.exceptions.request_exceptions import SwanHTTPError, SwanRequestError
+from typing import List, Dict, Any, Union
+from src.constants.constants import SWAN_API, ALL_CP_MACHINE, COLLATERAL_BALANCE
+from src.exceptions.request_exceptions import (
+    SwanHTTPError,
+    SwanRequestError,
+    SwanConnectionError,
+    SwanTimeoutError,
+)
 
 
 def get_all_cp_machines() -> List[Dict[str, Any]]:
@@ -42,3 +47,67 @@ def get_all_cp_machines() -> List[Dict[str, Any]]:
     except ValueError as json_err:
         logging.error(f"JSON decoding error: {json_err}")
         raise json_err
+
+
+def get_collateral_balance(cp_address: str) -> Dict[str, Union[str, Any]]:
+    """
+    Retrieves the collateral balance for a given computing provider address.
+
+    This function makes a GET request to the '/cp/collateral/<cp_address>' endpoint
+    to retrieve the collateral balance associated with the specified computing provider address.
+
+    Args:
+        cp_address (str): The computing provider address.
+
+    Returns:
+        dict: A dictionary containing the status, message, and data (balance) if successful,
+              or status and error message if an error occurs.
+
+    Raises:
+        ValueError: If the cp_address is not a valid address format.
+        ConnectionError: If there is a problem connecting to the API endpoint.
+        Exception: For other unforeseen errors.
+
+    Example:
+        get_collateral_balance("0x1234abcd")
+        {'status': 'success', 'message': 'Successfully retrieved collateral balance', 'data': {'balance': 100}}
+    """
+
+    # Endpoint configuration
+    endpoint = f"{SWAN_API}{COLLATERAL_BALANCE}{cp_address}"
+
+    try:
+        # Making a GET request to the API
+        response = requests.get(endpoint)
+        response.raise_for_status()  # Will raise an HTTPError for bad requests
+
+        # Parsing the JSON response
+        return response.json()
+
+    except requests.exceptions.HTTPError as http_err:
+        logging.error(f"HTTP error occurred: {http_err}")
+        raise SwanHTTPError(
+            "An HTTP error occurred while retrieving collateral balance"
+        ) from http_err
+
+    except requests.exceptions.ConnectionError as conn_err:
+        logging.error(f"Error connecting: {conn_err}")
+        raise SwanConnectionError(
+            "A connection error occurred while retrieving collateral balance"
+        ) from conn_err
+
+    except requests.exceptions.Timeout as timeout_err:
+        logging.error(f"Timeout error: {timeout_err}")
+        raise SwanTimeoutError(
+            "A timeout occurred while retrieving collateral balance"
+        ) from timeout_err
+
+    except requests.exceptions.RequestException as req_err:
+        logging.error(f"Error during requests to {endpoint}: {req_err}")
+        raise SwanRequestError(
+            "An unexpected error occurred while retrieving collateral balance"
+        ) from req_err
+
+    except Exception as e:
+        logging.error(f"An unexpected error occurred: {e}")
+        raise Exception(f"An unexpected error occurred: {e}")
