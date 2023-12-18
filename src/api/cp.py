a6996e839eb2a54137c047d32f943392c2fa7c42
arash khosravi <arash.khosravi@nbai.io>
2023-12-18 12:29:50 -0500

    Add get_computing_providers_list

--- src/api/cp.py
+++ src/api/cp.py
@@ -3,8 +3,8 @@
 import logging
 import requests
 from typing import List, Dict, Any
-from src.constants.constants import SWAN_API, ALL_CP_MACHINE
-from src.exceptions.request_exceptions import SwanHTTPError, SwanRequestError
+from src.constants.constants import SWAN_API, ALL_CP_MACHINE, CP_LIST
+from src.exceptions.request_exceptions import SwanHTTPError, SwanRequestError, SwanConnectionError, SwanTimeoutError
 
 
 def get_all_cp_machines() -> List[Dict[str, Any]]:
@@ -42,3 +42,48 @@ def get_all_cp_machines() -> List[Dict[str, Any]]:
     except ValueError as json_err:
         logging.error(f"JSON decoding error: {json_err}")
         raise json_err
+
+
+def get_computing_providers_list(region: str) -> List[Dict[str, Any]]:
+    """
+    Fetches a list of computing providers from the API based on the given region and hardware.
+
+    Args:
+        region (str): The region for which computing providers are requested.
+
+    Returns:
+        List[Dict[str, Any]]: A list of dictionaries containing details of computing providers.
+
+    Raises:
+        SwanHTTPError: If the API call fails.
+        SwanConnectionError: If there is a network problem connecting to the API.
+        SwanTimeoutError: If the request to the API times out.
+        ValueError: If the response from the API cannot be decoded.
+        Exception: For other types of exceptions.
+
+    """
+    if not region:
+        raise ValueError("Please provide a valid Region")
+
+    url = f"{SWAN_API}{CP_LIST}"
+    data = {"region": region}
+
+    try:
+        response = requests.post(url, data=data)
+        response.raise_for_status()
+        return response.json()['data']
+    except requests.exceptions.HTTPError as http_err:
+        logging.error(f"HTTP error occurred: {http_err}")
+        raise SwanHTTPError("HTTPError to connect to the API endpoint.")
+    except requests.exceptions.ConnectionError as connection_err:
+        logging.error("Failed to connect to the API endpoint.")
+        raise SwanConnectionError(f"Could not connect to the API endpoint {connection_err}.")
+    except requests.exceptions.Timeout as timeout_err:
+        logging.error("Request to the API endpoint timed out.")
+        raise SwanTimeoutError(f"The request to the API timed out {timeout_err}.")
+    except ValueError:
+        logging.error("Failed to decode the response from the API.")
+        raise ValueError("Invalid response received from the API.")
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        raise Exception(f"An error occurred: {e}")
