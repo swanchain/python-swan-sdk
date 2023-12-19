72928e1167a44ae6ee62d5ea9f7110d4216d8fdc
arash khosravi <arash.khosravi@nbai.io>
2023-12-19 14:13:54 -0500

    Clean constant

--- src/constants/constants.py
+++ src/constants/constants.py
@@ -2,10 +2,8 @@
 
 # Base APIs
 SWAN_API = "http://swanhub-cali.swanchain.io"
-AUCTION_API = "http://172.16.200.12:5003"
 
 # APIs
-
 TOKEN_VALIDATION = "/api_token/validate"
 STATS_GENERAL = "/stats/general"
 TASK_BIDDING = "/task/bidding"
