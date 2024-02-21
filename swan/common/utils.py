a92b7622d247f4df94d1783ca44389c46077499c
James-Chen1 <James.Chen@nbai.io>
2024-02-21 17:57:28 -0500

    added functionality to extract raw code from ipfs repos

--- swan/common/utils.py
+++ swan/common/utils.py
@@ -1,40 +1,51 @@
 # ./swan/common/utils.py
 import requests
 import re
-
+from urllib.parse import urlparse
 
 def parse_params_to_str(params):
     url = "?"
     for key, value in params.items():
         url = url + str(key) + "=" + str(value) + "&"
     return url[0:-1]
-
-def list_repo_contents(repo_url):
+def list_repo_contents(source_code_url):
     """
-    Lists the contents of a GitHub repository and gets the raw contents of the .env file, the Dockerfile, and all .yml files.
+    Lists the contents of a GitHub or IPFS repository and gets the raw contents of the .env file, the Dockerfile, and all .yml files.
 
     Parameters:
-    repo_url (str): The URL of the repository.
+    source_code_url (str): The URL of the repository.
 
     Returns:
     dict: A dictionary where the keys are the filenames and the values are the raw contents of the files.
     """
-    parts = repo_url.split('/')
-    user = parts[-2]
-    repo = parts[-1]
-
-    url = f"https://api.github.com/repos/{user}/{repo}/contents"
-    response = requests.get(url)
-    response.raise_for_status()
-    repo_contents = response.json()
-
     file_contents = {}
-    for item in repo_contents:
-        if item['type'] == 'file' and (item['name'] in ['.env', 'Dockerfile'] or re.search(r'\.yml$', item['name'])):
-            raw_url = item['download_url']
-            file_content = read_file_from_url(raw_url)
-            if file_content is not None:
-                file_contents[item['name']] = file_content
+
+    if 'github.com' in source_code_url:
+        # Handle GitHub links
+        # Extract the username and repository name from the URL
+        parts = source_code_url.split('/')
+        user = parts[-2]
+        repo = parts[-1]
+
+        url = f"https://api.github.com/repos/{user}/{repo}/contents"
+        response = requests.get(url)
+        response.raise_for_status()
+        repo_contents = response.json()
+
+        for item in repo_contents:
+            if item['type'] == 'file' and (item['name'] in ['.env', 'Dockerfile'] or re.search(r'\.yml$', item['name'])):
+                raw_url = item['download_url']
+                file_content = requests.get(raw_url).text
+                if file_content is not None:
+                    file_contents[item['name']] = file_content
+    else:
+        # Handle IPFS links
+        parsed_url = urlparse(source_code_url)
+        gateway_url = f"{parsed_url.scheme}://{parsed_url.netloc}/ipfs/"
+        file_hash = source_code_url.split('/')[-1]
+        response = requests.get(gateway_url + file_hash)
+        response.raise_for_status()
+        file_contents[file_hash] = response.text
 
     return file_contents
 
