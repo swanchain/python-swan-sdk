9104bf45447aeb6d0690696c8519037045d4914c
James-Chen1 <James.Chen@nbai.io>
2024-02-22 13:35:30 -0500

    updated tests

--- test/api/test_swan_api.py
+++ test/api/test_swan_api.py
@@ -34,17 +34,101 @@ class TestSwanAPI:
 
             assert result is None
 
-    def test_build_task(self):
-        task = self.swan_api.build_task()
+    @patch("swan.api.swan_api.list_repo_contents")
+    @patch("swan.api.swan_api.upload_file")
+    @patch("swan.api.swan_api.os")
+    @patch("swan.api.swan_api.uuid")
+    def test_prepare_task(
+        self, mock_uuid, mock_os, mock_upload_file, mock_list_repo_contents
+    ):
+        # Arrange
+        mock_os.getenv.return_value = "/tmp"
+        mock_uuid.uuid4.return_value = "random_uuid"
+        mock_list_repo_contents.return_value = {
+            ".env": "env_content",
+            "Dockerfile": "dockerfile_content",
+            "kubernetes.yaml": "kubernetes_content",
+        }
+        mock_upload_file.return_value = "mcs_file"
 
-    def test_build_task_invalid_data(self):
-        task = self.swan_api.build_task()
+        # Act
+        result = self.swan_api.prepare_task("source_code_url")
 
-    def test_propose_task(self):
-        task = self.swan_api.propose_task()
+        # Assert
+        mock_list_repo_contents.assert_called_once_with("source_code_url")
+        mock_upload_file.assert_called()
+        assert self.swan_api.task["random_uuid"] == "random_uuid"
+        assert self.swan_api.task["job_source_uri"] == "/tmp/spaces/random_uuid"
+        assert result is None
 
-    def test_propose_task_invalid_data(self):
-        task = self.swan_api.propose_task()
+    @patch("swan.api.swan_api.list_repo_contents")
+    @patch("swan.api.swan_api.upload_file")
+    @patch("swan.api.swan_api.os")
+    @patch("swan.api.swan_api.uuid")
+    def test_prepare_task_error(
+        self, mock_uuid, mock_os, mock_upload_file, mock_list_repo_contents
+    ):
+
+        mock_os.getenv.return_value = "/tmp"
+        mock_uuid.uuid4.return_value = "random_uuid"
+        mock_list_repo_contents.side_effect = Exception("Error")
+
+        result = self.swan_api.prepare_task("source_code_url")
+
+        assert result is None
+
+    @patch("swan.api.swan_api.SwanAPI._request_without_params")
+    @patch("swan.api.swan_api.SwanAPI._request_with_params")
+    def test_propose_task(self, mock_request_with_params, mock_request_without_params):
+
+        mock_request_without_params.return_value = {
+            "data": {
+                "hardware": [
+                    {"hardware_name": "config_name", "region": ["region1", "region2"]}
+                ]
+            }
+        }
+        mock_request_with_params.return_value = "result"
+        self.swan_api.task = {
+            "config_name": "config_name",
+            "duration": 10,
+            "paid_amount": 100,
+            "tx_hash": "tx_hash",
+            "job_source_uri": "job_source_uri",
+        }
+
+        result = self.swan_api.propose_task("start_in", "region1")
+
+        mock_request_without_params.assert_called_once()
+        mock_request_with_params.assert_called_once()
+        assert result == "result"
+
+    @patch("swan.api.swan_api.SwanAPI._request_without_params")
+    @patch("swan.api.swan_api.SwanAPI._request_with_params")
+    def test_propose_task_invalid_region(
+        self, mock_request_with_params, mock_request_without_params
+    ):
+
+        mock_request_without_params.return_value = {
+            "data": {
+                "hardware": [
+                    {"hardware_name": "config_name", "region": ["region1", "region2"]}
+                ]
+            }
+        }
+        self.swan_api.task = {
+            "config_name": "config_name",
+            "duration": 10,
+            "paid_amount": 100,
+            "tx_hash": "tx_hash",
+            "job_source_uri": "job_source_uri",
+        }
+
+        result = self.swan_api.propose_task("start_in", "invalid_region")
+
+        mock_request_without_params.assert_called_once()
+        mock_request_with_params.assert_not_called()
+        assert result is None
 
     def test_make_payment(self):
         payment = self.swan_api.make_payment()
