db9a7ebcd945c4567f42ab1d652a32993901d82d
James-Chen1 <James.Chen@nbai.io>
2024-02-22 13:04:03 -0500

    init setup for mcs sdk integration

--- swan/common/__init__.py
+++ swan/common/__init__.py
@@ -1,8 +1,7 @@
 # ./swan/common/__init__.py
 from swan_mcs import APIClient
+
 import os
-
-
 def init_mcs_api():
     return APIClient(
         os.getenv("MCS_API_KEY"), os.getenv("MCS_ACCESS_TOKEN"), "polygon.mainnet"
