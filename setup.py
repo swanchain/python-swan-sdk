2bc6a15bf64ccff285cf4b8dc5de63ea9e486f88
ZihangChenNBAI <zhchen@nbai.io>
2024-03-15 17:03:47 -0400

    added pypi setup

--- setup.py
+++ setup.py
@@ -1,54 +1,37 @@
 """ SwanSDK setup code """
 
-from setuptools import setup
+from setuptools import setup, find_packages
 from pathlib import Path
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / "PIPRELEASEDOC.md").read_text()
 
 
-# There are some placeholder for future needs
-# TODO: Delete/Modify placeholders as needed
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name="swan-sdk",
-    version="0.1.0",
-    author="FilSwan",  # Placeholder for author's name
-    author_email="author@email.com",  # Placeholder for author's email
-    install_requires=["web3==5.31.1", "requests==2.28.1", "requests_toolbelt==0.10.1"],
-    packages=["swan", "swan.common", "swan.api"],
-    license="MIT",
-    include_package_data=True,
-    description="A python software development kit for Swan services",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/username/package",  # Placeholder for URL
-    python_requires=">=3.10",  # Specifies the required Python version
-    classifiers=[
-        # Placeholder for classifiers
-        # EXAMPLE
-        # "Development Status :: 3 - Alpha",
-        # "Intended Audience :: Developers",
-        # "License :: OSI Approved :: MIT License",
-        # "Programming Language :: Python :: 3.10",
-    ],
-    entry_points={
-        "console_scripts": [
-            # Placeholder for entry points
-            # "our_command = our_package.module:main_function",
+        name="orchestrator-sdk",
+        version="0.1.0",
+        packages=find_packages(),
+        description="A python developer tool kit for Swan Orchestrator services.",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/swanchain/orchestrator-sdk",
+        author="Filswan",
+        author_email="zhchen@nbai.io",
+        license="MIT",
+        classifiers=[
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 3",
         ],
-    },
-    extras_require={
-        # Placeholder for extras
-        # "extra_feature": ["extra_dependency"],
-    },
-    package_data={
-        "package": [
-            # Placeholder for package data
-            # "data/*.txt",
-        ],
-    },
-    scripts=[
-        # Placeholder for scripts
-        # "bin/script.py",
-    ],
-)
+        install_requires=[
+            "requests==2.28.1",
+            "requests-toolbelt==0.10.1",
+            "web3==6.15.1",
+            "tqdm==4.64.1"
+            ],
+        entry_points={
+            # placeholder
+        },
+        )
