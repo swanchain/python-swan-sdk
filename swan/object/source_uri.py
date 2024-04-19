240f06dfe83620a58d62e58a2771f70bf63e4659
ZihangChenNBAI <zhchen@nbai.io>
2024-04-19 17:33:41 -0400

    added github repo support

--- swan/object/source_uri.py
+++ swan/object/source_uri.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import uuid
 from swan.api.mcs_api import MCSAPI, File
 from swan.common.utils import datetime_to_unixtime
 from swan.object.cp_config import HardwareConfig
@@ -247,6 +248,104 @@ class LagrangeSpace():
         return json.dumps(self, default=lambda o: o.__dict__,
                           sort_keys=True, indent=4)
     
+    def generate_source_uri(self, bucket_name: str, obj_name: str, file_path: str, mcs_client: MCSAPI = None, replace: bool = True):
+        """Generate source uri for task using MCS service.
+
+        Args:
+            bucket_name: bucket name to store source uri.
+            obj_name: object name (dir + file name) to store source uri.
+            file_path: local file path to store source uri JSON file.
+            replace: replace existing file or not.
+            mcs_client: mcs API object.
+
+        Returns：
+            API response from MCS after uploading. Contains file information.
+
+        """
+        if mcs_client:
+            self.mcs_connection(mcs_client)
+            with open(file_path, "w") as file:
+                json.dump(self.to_dict(), file)
+            res = mcs_client.upload_file(bucket_name, obj_name, file_path, replace)
+            self.source_uri = res.ipfs_url
+            return res
+        
+class GithubRepo():
+
+    def __init__(self, repo_owner: str, repo_name: str, repo_branch: str,  wallet_address: str, hardware_config: HardwareConfig, repo_uri: str = None):
+    
+        if repo_uri and not (repo_owner or repo_name):
+            # TO DO: Add retrieve repo owner, name and branch from URI
+            pass
+
+        self.repo_owner = repo_owner
+        self.repo_name = repo_name
+        self.repo_branch = repo_branch
+        self.wallet_address = wallet_address
+        self.hardware_config = hardware_config
+        self.repo_uuid = str(uuid.uuid4())
+        self.mcs_client = None
+        self.source_uri = None
+        self.file_list = []
+
+    def mcs_connection(self, mcs_client: MCSAPI):
+        """Add mcs connection.
+
+        Args:
+            mcs_client: mcs API object.
+        """
+        self.mcs_client = mcs_client
+    
+    def get_github_tree(self):
+        github_tree_uri = f"https://api.github.com/repos/{self.repo_owner}/{self.repo_name}/git/trees/{self.repo_branch}?recursive=1"
+
+        github_repo_files = requests.get(github_tree_uri).json()
+
+        for file in github_repo_files["tree"]:
+            if file["type"] == "blob":
+                self.file_list.append(
+                    {
+                        "cid": file["sha"],
+                        "created_at": None,
+                        "name": file["path"],
+                        "updated_at": None,
+                        "url": f"https://raw.githubusercontent.com/{self.repo_owner}/{self.repo_name}/{self.repo_branch}/{file['path']}"
+                    }
+                )
+    
+    def to_dict(self):
+        hardware_info = self.hardware_config.description
+        hardware_info = hardware_info.split(" · ")
+
+        return {
+            "data": {
+                "files": self.file_list,
+                "owner": {
+                    "public_address": self.wallet_address,
+                },
+                "space": {
+                    "activeOrder": {
+                        "config": {
+                            "description": self.hardware_config.description,
+                            "hardware": hardware_info[0],
+                            "hardware_id": int(self.hardware_config.id),
+                            "hardware_type": self.hardware_config.type,
+                            "memory": [int(s) for s in hardware_info[2].split() if s.isdigit()][0],
+                            "name": self.hardware_config.name,
+                            "price_per_hour": float(self.hardware_config.price),
+                            "vcpu": [int(s) for s in hardware_info[1].split() if s.isdigit()][0],
+                        },
+                    },
+                    "name": self.repo_name,
+                    "uuid": self.repo_uuid,
+                }
+            }
+        }
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                          sort_keys=True, indent=4)
+
     def generate_source_uri(self, bucket_name: str, obj_name: str, file_path: str, mcs_client: MCSAPI = None, replace: bool = True):
         """Generate source uri for task using MCS service.
 
